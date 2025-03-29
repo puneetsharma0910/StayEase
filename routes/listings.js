a8428b9591596d75dfadc944f11b955bda25d14f
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-03-29 23:25:55 +0530

    Resolved route handling issue in listings.js by correcting the POST method implementation

--- routes/listings.js
+++ routes/listings.js
@@ -5,16 +5,16 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema } = require("../schema.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const listingController = require("../controllers/listing.js");
+const listingController = require("../controllers/listings.js");
 
 // Route to display all listings
-router.get(
-  "/",
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
 
-  wrapAsync(listingController.index)
-);
 
-//new route
+//new route`
 router.get(
   "/new",
   isLoggedIn,
@@ -22,16 +22,20 @@ router.get(
   wrapAsync(listingController.renderNewForm)
 );
 
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
 //show route
-router.get("/:id", wrapAsync(listingController.showListing));
 
 //crete route
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
 //edit route
 router.get(
   "/:id/edit",
@@ -40,20 +44,6 @@ router.get(
   wrapAsync(listingController.renderEditForm)
 );
 //update route
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing)
-);
-
 //delete route
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.destroyListing)
-);
 
 module.exports = router;
